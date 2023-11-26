db26ea48c18f30ebe4b469d5f516519cf50b3bd7
jiwon <leeji.circle@gmail.com>
2023-11-26 19:47:37 +0900

    -

--- Simple_Weather/weatherViewController.swift
+++ Simple_Weather/weatherViewController.swift
@@ -8,11 +8,34 @@
 import UIKit
 
 class weatherViewController: UIViewController {
+    @IBOutlet weak var cityLabel: UILabel!
     
-    let city = [""]
+    @IBOutlet weak var weatherImageView: UIImageView!
+    
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    @IBAction func changeButtonTapped(_ sender: Any) {
+        //도시 온도 날씨이미지 변경
+        cityLabel.text = Cities.randomElement()
+        
+        let imageName = DayWeathers.randomElement()!
+        weatherImageView.image = UIImage(systemName: imageName)?.withRenderingMode(.alwaysOriginal)
+        
+        let randomTemperature = Int.random(in: 10..<30)
+        temperatureLabel.text = "\(randomTemperature)°"
+        
+    }
+    
+    
+    let Cities = ["SEOUL","BUSAN","CHANGWON","INCHEON"]
+    
+    let DayWeathers = ["sun.max.fill","cloud.fill"
+        ,"wind","cloud.snow.fill"]
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
         // Do any additional setup after loading the view.
     }
